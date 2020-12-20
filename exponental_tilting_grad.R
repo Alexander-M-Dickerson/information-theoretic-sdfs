33f4478af30297c8ffbf11fb18456bfc283a39a7
Alex Dickerson <56037913+Alexander-M-Dickerson@users.noreply.github.com>
2020-12-20 11:24:34 +0000

    Update exponental_tilting_grad.R

--- exponental_tilting_grad.R
+++ exponental_tilting_grad.R
@@ -1,4 +1,4 @@
-exponental_tilting_grad <- function( par, ffq, prec=0 )     {
+exponential_tilting_grad <- function( par, ffq, prec=0 )     {
   Xbeta<-    exp(ffq%*%par) 
   t(ffq) %*% exp(ffq%*%par) 
 }
